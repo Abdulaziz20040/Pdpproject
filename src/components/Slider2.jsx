bfe39dbf718a2b276867802cdb0cff8369137bda
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-02-17 09:04:01 +0500

    ass

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import img1 from "./Group 202.png";
 
 function Slider2() {
-  return <div>Slider2</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://715c33c88529aa9a.mokky.dev/intefra")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="w-full mb-32">
+        <div className="flex justify-center items-center">
+          {data.slice(0, 1).map((item) => (
+            <div className="relative w-full h-[805px]" key={item.id}>
+              <img
+                src={item.img[0]}
+                alt=""
+                className="w-full h-full object-cover filter brightness-50"
+              />
+              <div className="absolute top-5 left-[60%] text-white px-4 py-2 rounded">
+                <button className="rounded-[35px] text-[20px] font-thin p-3 px-[50px] border-[1px] border-[#FFFFFF] ">
+                  Парфюм
+                </button>
+              </div>
+              <div className="absolute bottom-5 left-[40%] text-end text-white px-4 py-2 rounded">
+                <p className="mb-2 text-[30px] font-thin leading-[45px]">
+                  Maison
+                </p>
+                <p className="text-[30px] font-thin leading-[45px]">
+                  Francis Kurkdjian
+                </p>
+                <p className="text-[30px] font-thin leading-[45px]">
+                  Baccarat Rouge 540
+                </p>
+                <button className="border-b-[1px] border-white text-white px-4 py-2">
+                  Смотреть
+                </button>
+              </div>
+            </div>
+          ))}
+          {data.slice(1, 2).map((item) => (
+            <div className="relative w-full h-[805px]" key={item.id}>
+              <img
+                src={item.img[1]}
+                alt=""
+                className="w-full h-full object-cover filter brightness-50"
+              />
+              <div className="absolute top-5 left-5  text-black px-4 py-2">
+                <button className="rounded-[35px] text-[20px] font-thin p-3 px-8 border-[1px] border-[#333333] ">
+                  Запах месяца
+                </button>
+              </div>
+              <div className="absolute bottom-5 left-[75px] text-white px-4 py-2 rounded">
+                <p className="mb-2 text-[30px] font-thin leading-[45px]">
+                  Dr.Vranjes <br /> Rosso Nibile
+                </p>
+                <button className="border-b-[1px] border-white text-white px-4 py-2 ">
+                  Смотреть
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-[100%] mb-20">
+        <img src={img1} alt="" />
+      </div>
+    </>
+  );
 }
 
 export default Slider2;
--- src/pages/Oformit_aboout.jsx
+++ src/pages/Oformit_aboout.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import img2 from "./artigiani.png";
+import img3 from "./artigiani 1.png";
 
 function Oformit_Aboout() {
-  return <div>Oformit_Aboout</div>;
+  return (
+    <div className="relative w-full mb-32 h-[805px]">
+      {/* Background Image */}
+      <img src={img2} alt="" className="w-full h-full object-cover" />
+
+      {/* Content */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white">
+        <img
+          src={img3}
+          alt="Logo"
+          className="mb-4 w-[450px] h-auto object-contain"
+        />
+        <p className="mb-4 text-lg font-thin max-w-[700px]">
+          Изделия премиального качества из самых лучших материалов, ориентируясь
+          на традиции итальянских производителей.
+        </p>
+        <button className="px-6 py-5 text-[20px] font-thin text-white border-b-[1px]">
+          Cмотреть КОЛЛЕКЦИЮ
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Oformit_Aboout;
